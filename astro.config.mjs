f905a0b90a4360dc828d138dfb4866875e6a35f6
tkskto <tkskto@gmail.com>
2025-06-01 13:13:23 +0900

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  build: {
+    format: 'preserve',
+  },
+
+  adapter: vercel(),
+});
